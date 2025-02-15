1202ee765e39a4ff394bc967a823075dfdfde4cc
Om-ShindeDesai <103118435+Om-ShindeDesai@users.noreply.github.com>
2025-02-15 17:08:17 +0530

    Delete BankingPortal directory

--- BankingPortal/src/main/java/com/webapp/bankingportal/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.webapp.bankingportal.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.webapp.bankingportal.Entity.User;
-
-public interface UserRepository extends JpaRepository<User ,Long> {
-
-}
--- BankingPortal/src/main/java/com/webapp/bankingportal/service/AccountService.java
+++
@@ -1,9 +0,0 @@
-package com.webapp.bankingportal.service;
-
-import com.webapp.bankingportal.Entity.Account;
-import com.webapp.bankingportal.Entity.User;
-
-public interface AccountService {
-	public Account createAccount(User user);
-
-}
